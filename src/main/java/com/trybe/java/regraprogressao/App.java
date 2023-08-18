6bdb69c12ba41b96fae100d8b0c5b4c9c2578d09
rcamarano <rogeriocamarano@gmail.com>
2023-08-18 11:46:54 -0300

    starting the project

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -5,10 +5,6 @@ package com.trybe.java.regraprogressao;
  */
 public class App {
 
-  /**
-   * Metodo main.
-   * starting the project
-   */
   public static void main(String[] args) {
     System.out.println("Hello Java World");
   }
