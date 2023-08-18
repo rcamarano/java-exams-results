29e24dc3f2986abc2c445e49b8252af753630070
rcamarano <rogeriocamarano@gmail.com>
2023-08-18 12:09:55 -0300

    starting calculator structure

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,11 +1,34 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
 /**
  * App.
  */
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("Hello Java World");
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int activityQuantity = Integer.parseInt(scanner.nextLine());
+
+    int gradesSum = 0;
+
+    int weightSum = 0;
+
+    for (int i = 0; i < activityQuantity; i++) {
+      System.out.printf("Digite o nome da atividade %d:%n", i + 1);
+      String activityName = scanner.nextLine();
+
+      System.out.printf("Digite o peso da atividade %d:%n", i + 1);
+      int activityWeight = Integer.parseInt(scanner.nextLine());
+      weightSum += activityWeight;
+
+      System.out.println("Digite a nota obtida para " + activityName + ":");
+      int grade = Integer.parseInt(scanner.nextLine());
+      gradesSum += activityWeight * grade;
+    }
+
+    float finalGradeResult = (float) gradesSum / weightSum;
   }
 }
